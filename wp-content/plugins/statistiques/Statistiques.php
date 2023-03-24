416f1b72b3c06ddc118e94088b74040d87e54f73
cedwics10 <brochettes40@outlook.fr>
2023-03-24 10:44:19 +0100

    module stats updated

---
+++ wp-content/plugins/statistiques/Statistiques.php
@@ -0,0 +1,119 @@
+<?php
+class statistiques
+{
+    private const PARAMS_EXPLODE = [',', ' ', '.', ')', '('];
+
+    private array $mots_inutiles;
+    private array $occurences = [];
+    private array $nb_mots_phrase;
+
+    private string $articleContent = '';
+    private string $contenu = '';
+    private string $type;
+
+
+    public function __construct()
+    {
+        $this->mots_inutiles = explode("\r\n", trim(file_get_contents(dirname(__FILE__) . '/mots_inutiles.txt')));
+        $this->contenu = get_the_content();
+        $this->computeType();
+    }
+
+    public function computeStats()
+    {
+        $this->lengthSentences();
+        $this->frequencylistW();
+    }
+
+    private function computeType()
+    {
+        switch (get_post_type()) {
+            case 'page':
+                $this->type = "la page";
+                break;
+            case 'post':
+                $this->type = "l'article";
+                break;
+            default:
+                $this->type = 'inconnu';
+                break;
+        }
+    }
+
+    private function lengthSentences()
+    {
+        $phrase_page = explode('.', $this->contenu);
+        $this->nb_mots_phrase = array_fill(0, count($phrase_page), 0);
+        foreach ($phrase_page as $id => $phrase) {
+            $mots = explode(' ', $phrase);
+            foreach ($mots as $mot) {
+                if (in_array($mot, $this->mots_inutiles)) {
+                    continue;
+                }
+                $this->nb_mots_phrase[$id]++;
+            }
+        }
+        arsort($this->nb_mots_phrase, SORT_NUMERIC);
+    }
+
+    private function frequencylistW()
+    {
+        $mots_page = multiexplode(
+            self::PARAMS_EXPLODE,
+            $this->contenu
+        );
+
+        foreach ($mots_page as $mot) {
+            if (
+                in_array(strtolower($mot), $this->mots_inutiles)
+                or empty(trim($mot))
+            ) {
+                continue;
+            }
+            $this->occurences[$mot] = $this->occurences[$mot] ?? 0;
+            $this->occurences[$mot]++;
+        }
+        arsort($this->occurences, SORT_NUMERIC);
+    }
+
+    private function topTenWords()
+    {
+        $i = 1;
+        foreach ($this->occurences as $mot => $nbOccurence) {
+            echo "$mot ($nbOccurence), ";
+            if ($i == 10) break;
+            $i++;
+        }
+    }
+
+    private function topTenSentence()
+    {
+        foreach ($this->nb_mots_phrase as $phrase => $nbr) {
+            $s = '';
+            if ($nbr > 1)
+                $s  = 's';
+            echo "La phrase #$phrase a $nbr mot$s.<br />";
+        }
+    }
+
+    public function afficherStats()
+    {
+        $lenContent = mb_strlen($this->contenu);
+        if ($lenContent == 0)
+            return 'Le texte est vide';
+
+        echo "<div id='stats'>Ici les statistiques de $this->type !<br /><br >
+        - Longueur du contenu : " .  $lenContent . "<br />
+        - Les 10 mots les plus utilisés de $this->type<br />";
+
+        $this->topTenWords();
+
+        echo '<br />';
+
+        echo 'Top des phrases les plus longues :<br />';
+
+        echo $this->topTenSentence();
+
+        echo '<br /></div>';
+    }
+}
--- wp-content/plugins/statistiques/index.php
+++ wp-content/plugins/statistiques/index.php
@@ -8,6 +8,16 @@ Version: 1.0.0
 Author URI: http://cedwicsten/
 */
 
+function multiexplode($delimiters, $string)
+{
+
+    $ready = str_replace($delimiters, $delimiters[0], $string);
+    $launch = explode($delimiters[0], $ready);
+    return  $launch;
+}
+
+include 'Statistiques.php';
+
 //Ajouter une entrée de menu dans le tableau de bord
 add_action("admin_menu", "charger_menu_stats");
 
@@ -25,80 +35,10 @@ function fct_page_stats()
 //Déclénche une fonction avant l'affichage de l'entete de page.
 add_action('get_header', 'affiche_stats');
 
-function multiexplode($delimiters, $string)
-{
-
-    $ready = str_replace($delimiters, $delimiters[0], $string);
-    $launch = explode($delimiters[0], $ready);
-    return  $launch;
-}
 
 function affiche_stats()
 {
-    $dir  = 'C:\Portfolio\gitare\wp-content\plugins\statistiques\\';
-    $mots_inutiles = explode("\ r\n", trim(file_get_contents(dirname(__FILE__) . '/mots_inutiles.txt')));
-
-    $contenu = get_the_content();
-    switch (get_post_type()) {
-        case 'page':
-            $type = "la page";
-            break;
-        case 'post':
-            $type = "l'article";
-            break;
-        default:
-            $type = 'inconnu';
-            break;
-    }
-
-    echo "<div id='stats'>Ici les statistiques de $type !<br /><br />";
-
-
-    $occurences = [];
-    echo 'Longueur du texte : ' . strlen($contenu) . '<br />';
-
-    $phrase_page = explode('.', $contenu);
-    $occ_phrases_page = array_fill(0, count($phrase_page), 0);
-
-    $mots_page = multiexplode([',', ' ', '.', ')', '('], $contenu);
-
-    foreach ($mots_page as $mot) {
-        if (in_array($mot, $mots_inutiles)) {
-            continue;
-        }
-        $occurences[$mot] = $occurences[$mot] ?? 0;
-        $occurences[$mot]++;
-    }
-
-    foreach ($phrase_page as $id => $phrase) {
-        $mots = explode(' ', $phrase);
-        foreach ($mots as $mot) {
-            if (in_array($mot, $mots_inutiles)) {
-                continue;
-            }
-            $occ_phrases_page[$id]++;
-        }
-    };
-
-    arsort($occ_phrases_page, SORT_NUMERIC);
-    foreach ($occ_phrases_page as $phrase => $occ) {
-        echo "Phrase #$phrase : $occ, ";
-    }
-    echo '<br />';
-
-    arsort($occurences, SORT_NUMERIC);
-
-    echo 'Les 10 mots les plus utilisés de l\'article : ';
-
-    $nbMots = 10;
-    $i = 1;
-    foreach ($occurences as $mot => $nbOccurence) {
-        echo "$mot ($nbOccurence),";
-        if ($i == 10) break;
-        $i++;
-    }
-
-    echo '</pre>';
-
-    echo '</div>';
+    $mesStats = new Statistiques();
+    $mesStats->computeStats();
+    $mesStats->afficherStats();
 }
